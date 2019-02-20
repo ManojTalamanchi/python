600253faab92e60a0256617d92e51e9c6db22090
ManojTalamanchi <42737261+ManojTalamanchi@users.noreply.github.com>
2019-02-20 01:44:25 -0800

    Update special character.py

--- special character.py
+++ special character.py
@@ -1,6 +1,9 @@
-name=input("")
-count=1
-for j in name:
-    if(j.isspace()):
-        count=count+1
-print(count)
+symbol = "~`!@#$%^&*()_-+={}[]:>;',</?*-+1234567890"
+name = str(input())
+s=list(symbol)
+c=1
+l=list(name)
+for i in range(0,len(l)):
+    if l[i] in s:
+      c=c+1
+print(c)
