2719dee13050bba06d3113f7b9c3e998e154c35c
ManojTalamanchi <42737261+ManojTalamanchi@users.noreply.github.com>
2019-02-20 21:40:06 -0800

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+m=int(input(""))
+m1 = 0
+m2 = 1
+count = 0
+n=[]
+if m == 1:
+    n.append(m1)
+    print(n)
+else:
+    while count < m:
+        n.append(m1)
+        mth = m1 + m2
+        m1 = m2
+        m2 = mth
+        count += 1
+print(" ".join(str(x) for x in n))
