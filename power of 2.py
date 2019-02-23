4393291e29965499ac619dc28059d5ad7babb80c
ManojTalamanchi <42737261+ManojTalamanchi@users.noreply.github.com>
2019-02-22 22:22:57 -0800

    Update power of 2.py

--- power of 2.py
+++ power of 2.py
@@ -1,16 +1,13 @@
-def main():
-	try:
-		g=0
-		b=int(input())
-		while(b!=0):
-			b=b/2
-			if b==1:
-				g=1
-				break
-		if g!=1:
-			print('no')
-		else :
-			print('yes')
-	except:
-		print('invalid')
-main()
+g=0
+b=int(input())
+while(b>0):
+  if b==1:
+	  g=1
+	  break
+  else:
+    b=b/2
+
+if g!=1:
+	print('no')
+else :
+	print('yes')
